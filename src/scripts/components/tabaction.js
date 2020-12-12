5f6390d3461f843d306266b9fa54256065642720
Vitali-FutureDeveloper <zarvoron@yandex.ru>
2020-12-12 23:17:20 +0500

    Рефактор и документирование кода Табов

--- src/scripts/components/tabaction.js
+++ src/scripts/components/tabaction.js
@@ -1,8 +1,23 @@
-window.tabaction = function(buttons, blocks, i, ACTIVE_CLASS, ANIMATION_CLASS=false) {
-	blocks.forEach((item) => item.classList.remove(ACTIVE_CLASS));
-	blocks[i].classList.add(ACTIVE_CLASS);
-	if(ANIMATION_CLASS) {
-		blocks.forEach((item) => item.classList.remove(ANIMATION_CLASS));
-		blocks[i].classList.add(ANIMATION_CLASS);
-	}
-};
+(function () {
+
+	window.tabaction = function(obj, i) {
+		/**
+		 * Компонент : src\scripts\components\tabaction.js
+		 * ТАБЫ (движок): Замена отображения блока в зависимости от нажатой кнопки
+		 * @param obj / {
+		 *   buttons : [] - кнопки для нажатий,
+		 *   blocks : [] - блоки на которые показываем в зависимости о номера нажатой кнопки,
+		 *   active_class : string - задаваемый класс для активного блока для показа,
+		 *   animation_class : string - задаваемый класс для активного блока для анимации,
+		 * }
+		 * @param i : number - номер нажатой кнопки из цикла с делегированием событий,
+		 */
+		obj.blocks.forEach((item) => item.classList.remove(obj.active_class));
+		obj.blocks[i].classList.add(obj.active_class);
+		if(obj.animation_class) {
+			obj.blocks.forEach((item) => item.classList.remove(obj.animation_class));
+			obj.blocks[i].classList.add(obj.animation_class);
+		}
+	};
+
+})();
--- src/scripts/interactive/scale-tab.js
+++ src/scripts/interactive/scale-tab.js
@@ -2,20 +2,52 @@
 	const ACTIVE_CLASS = 'about__text--active';
 	const ANIMATION_CLASS = 'tab-animation';
 	const ACTIVE_BLOCK = 'scale__block--active';
+
 	var buttonsWrap = document.querySelector('.scale');
 	var buttons = document.querySelectorAll('.scale__button');
 	var blocks = document.querySelectorAll('.about__text');
 
-	buttonsWrap.addEventListener('click', function (evt) {
+	buttonsWrap.addEventListener('click', onChangeTabs);
+
+	function onChangeTabs(evt) {
+		/**
+		 * Обработчик с делегировнием событий: на блок с кнопками
+		 * @param evt {} - event object
+		 */
 		for (let i = 0; i < buttons.length; i++) {
 			if (evt.target.classList.contains('scale__button') && evt.target == buttons[i]){
-				window.tabaction(buttons, blocks, i, ACTIVE_CLASS, ANIMATION_CLASS);
-				onChangeButton(i);
+				/*
+				 * Компонент : src\scripts\components\tabaction.js
+				 * ТАБЫ (движок): Замена отображения блока в зависимости от нажатой кнопки
+				 * @param obj {
+				 *   buttons : [] - кнопки для нажатий,
+				 *   blocks : [] - блоки на которые показываем в зависимости о номера нажатой кнопки,
+				 *   active_class : string - задаваемый класс для активного блока для показа,
+				 *   animation_class : string - задаваемый класс для активного блока для анимации,
+				 * }
+				 * @param i : number - номер нажатой кнопки из цикла с делегированием событий,
+				 */
+				window.tabaction({
+					buttons : buttons,
+					blocks : blocks,
+					active_class : ACTIVE_CLASS,
+					animation_class : ANIMATION_CLASS,
+				}, i);
+
+				/*
+				 * Меняет класс родителя кнопки на статус активный
+				 * @param i : number - номер нажатой кнопки из цикла с делегированием событий,
+				 */
+				сhangeButton(i);
 			}
 		}
-	});
+	}
 
-	function onChangeButton(i){
+	function сhangeButton(i){
+		/**
+		 * Меняет класс родителя кнопки на статус активный
+		 * @param i : number - номер нажатой кнопки из цикла с делегированием событий,
+		 */
 		buttons.forEach((item) => item.parentNode.classList.remove(ACTIVE_BLOCK));
 		buttons[i].parentNode.classList.add(ACTIVE_BLOCK);
 	}
